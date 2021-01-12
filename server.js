8fa0cd8d1ae58899d91842f77f0f12392c6aac7a
liam cohen <LCmanutd@gmail.com>
2021-01-12 16:18:46 +0200

    added

---
+++ server.js
@@ -0,0 +1,58 @@
+//server.js
+const express = require("express");
+const app = express();
+const { port } = require("./config");
+var bodyParser = require('body-parser')
+var cors = require('cors');
+app.use(cors());
+
+jsonParser = bodyParser.json();
+
+var DB = [
+    { category: 'פירות', name:'אבוקדו', number:3, price:5, totalPrice: 15 },
+    { category: 'מוצרי חלב', name:'מילקי', number:6, price:4, totalPrice: 24 }
+  ];
+
+// http://localhost:3001/
+app.get('/', async (request, response) => {
+    response.status(200).send({
+        message: "Hello from Express Server!"
+    });
+});
+
+// http://localhost:3001/getInitialList
+app.get('/getInitalList', async function (request, response) {
+    var code = 200;
+
+    response.status(code).send({
+        message: DB
+    });
+});
+
+// http://localhost:3001/addItem
+app.post('/addItem', jsonParser, async (request, response) => {
+    var code = 200;
+    var content = "Doesnt Exists";
+    var body = request.body;
+
+    var prom = new Promise((res, rej) => {
+        for (i = 0; i < DB.length; i++) {
+            if (DB[i].category === body.category && DB[i].name === body.name) {
+                code = 400;
+                content = "Exists";
+                res("Exists");
+            }
+        }
+        res("Doesnt Exists");
+    });
+    
+    prom.then(value => {
+        if(value === "Doesnt Exists") DB.push(body);
+    })
+
+    response.status(code).send({
+        message: content
+    });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
